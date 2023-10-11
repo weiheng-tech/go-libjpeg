fdaa54e49b1831baf70000686b2b1ec16881971b
guoxi.wang <guoxi.wang@weiheng-tech.com>
2023-10-11 19:39:30 +0800

    fix: add support govendor

---
+++ jpeg/config.go
@@ -0,0 +1,8 @@
+package jpeg
+
+import (
+	_ "github.com/ssbeatty/go-libjpeg/jpeg/include"
+	_ "github.com/ssbeatty/go-libjpeg/jpeg/lib/amd64"
+	_ "github.com/ssbeatty/go-libjpeg/jpeg/lib/armv7l"
+	_ "github.com/ssbeatty/go-libjpeg/jpeg/lib/win"
+)
